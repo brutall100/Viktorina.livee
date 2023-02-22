f6a941dfb0bc870e954f282befdc3adedd078039
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-02-22 10:45:20 +0200

    Update Uzrasi.php

--- OldFiles_NotUse/Uzrasi.php
+++ OldFiles_NotUse/Uzrasi.php
@@ -1,66 +1,23 @@
-<?php
-  // Connect to the database
-  $host = 'localhost';
-  $user = 'root';
-  $password = '';
-  $dbname = 'viktorina';
+<!-- 1 uzduotis -->
+<!-- Sutvarkyri perejimus is vieno puslapio i kita, su prisijungimu -->
 
-  $conn = mysqli_connect($host, $user, $password, $dbname);
+<!-- 2 -->
+<!-- Pasidaryti svaria php ir javaScript registracijos kopija githube su pasijungimu i xampp -->
 
-  // Check connection
-  if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-  }
+<!-- 3 -->
+<!-- Didelis darbas atsakymas i klausima tasku gavimas,bonus gavimas -->
 
-  // Select the data from the database
-  $sql = "SELECT id, user, question, answer, vote_count, date FROM viktorina.question_answer";
-  $result = mysqli_query($conn, $sql);
+<!-- 4 -->
+<!-- Question waiting sutvarkyti, kad klausimai su upvotais pereitu i pagrindine db -->
 
-  if (mysqli_num_rows($result) > 0) {
-      // Output the data
-      while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row['id'] . "</td>";
-        echo "<td>" . $row['user'] . "</td>";
-        echo "<td>" . $row['question'] . "</td>";
-        echo "<td>" . $row['answer'] . "</td>";
-        echo "<td>
-        <button class='upvote' data-id='". $row['id'] ."'>Upvote</button>
-        </td>";
-        echo "<td>
-        <button class='downvote' data-id='". $row['id'] ."'>Downvote</button>
-        </td>";
-        echo "<td class='vote_count " . ($row['vote_count'] >= 0 ? 'positive' : 'negative') . "'>" . $row['vote_count'] . "</td>";
+<!-- 5 -->
+<!-- a_index.php pradetu imti klausimus is pagrindines db -->
 
-        // Check if the question is older than a week and has a positive vote count
-        $date = new DateTime($row['date']);
-        $now = new DateTime();
-        $interval = $now->diff($date);
-        if($interval->days >= 7 && $row['vote_count'] > 0) {
-          // Insert the question into the super_database
-          $sql = "INSERT INTO viktorina.super_database (id, question, answer) VALUES ('".$row['id']."','".$row['question']."','".$row['answer']."')";
-          mysqli_query($conn, $sql);
-          // Delete the question from the question_answer table
-          $sql = "DELETE FROM viktorina.question_answer WHERE id = '".$row['id']."'";
-          mysqli_query($conn, $sql);
-        }
-        // Check if the question is older than a week and has a negative vote count
-        if($interval->days >= 7 && $row['vote_count'] < 0) {
-          // Delete the question from the question_answer table
-          $sql = "DELETE FROM viktorina.question_answer WHERE id = '".$row['id']."'";
-          mysqli_query($conn, $sql);
-        }
-    }
-        
-  } else {
-    echo "0 results";
-}
+<!-- 6 -->
+<!-- Padarti i statistic.php menesio top savaites top dienos top nusinulina 12 nakties -->
 
-// Close the connection
-mysqli_close($conn);
-?>
-
-
+<!-- 7 -->
+<!-- Full screen api ,kad vaizdas butu per visa ekrana -->
 
 <!-- Recapha    apsauga -->
 reCAPTCHA is a free service from Google that helps protect websites from spam and abuse. It uses advanced risk analysis techniques to tell humans and bots apart. With the new API, a significant number of your valid human users will pass the reCAPTCHA challenge without having to solve a CAPTCHA. reCAPTCHA offers more than just spam protection. It can also be used for security purposes like to prevent bots from trying to login to your site.
