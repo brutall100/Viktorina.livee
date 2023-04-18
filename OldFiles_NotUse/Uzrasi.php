f4ca7946be8b7180892831e81c5431c7098553f2
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-04-18 17:57:20 +0300

    V-3.5.9
    Pridetas HTML prie php codo

--- OldFiles_NotUse/Uzrasi.php
+++ OldFiles_NotUse/Uzrasi.php
@@ -57,6 +57,18 @@ nusipirkti is storo uz litus,plius gender ikoneles.Taippat galima pirkti store -
 <!-- 19 -->
 <!--Game2  Reikia ziureti ka jis ten skaiciuoja. Nes tai prideda tai atima -->
 
+<!-- 20 -->
+<!-- b_newquestion.php cia iskylantis phhp puslapis reikia sutvarkyti zinute, prideti css pagrazinti,
+pridet laika beganti atgal, kad esi nukreipiamas atgal i klausimo irasymo page, prideti zvaigzdute klausimo irasytojas -->
+
+
+
+
+
+
+
+
+
 <!-- Recapha    apsauga -->
 reCAPTCHA is a free service from Google that helps protect websites from spam and abuse. It uses advanced risk analysis techniques to tell humans and bots apart. With the new API, a significant number of your valid human users will pass the reCAPTCHA challenge without having to solve a CAPTCHA. reCAPTCHA offers more than just spam protection. It can also be used for security purposes like to prevent bots from trying to login to your site.
 
--- b_newquestion.php
+++ b_newquestion.php
@@ -1,83 +1,107 @@
-<?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$dbname = 'viktorina';
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Naujo klausimo įrašymas.</title>
+	<link rel="stylesheet" href="b_newquestion.css">
+</head>
+<body>
 
-$conn = mysqli_connect($host, $user, $password, $dbname);
+	<div class="message">
+    <?php
+      $host = 'localhost';
+      $user = 'root';
+      $password = '';
+      $dbname = 'viktorina';
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+      $conn = mysqli_connect($host, $user, $password, $dbname);
 
-$name = $_POST['name'];
-$user_id = $_POST['user_id'];
-$question = $_POST['question'];
-$answer = $_POST['answer'];
-$date_inserted = date("Y-m-d"); // current date
-$ip = $_SERVER['REMOTE_ADDR'];
+      if (!$conn) {
+          die("Connection failed: " . mysqli_connect_error());
+      }
 
-$message = "Naujas klausimas sukurtas sėkmingai. Klausimas irašytas į laikinają duomenų bazę. Kur bus apdorojamas. Už įrašyta klausimą jums suteikta 10 LITŲ.";
-if (empty($question) || empty($answer)) {
-    $message = "Klaida: ne visi laukai užpildyti. Klausimas ir atsakymas yra būtini.";
-}
+      $name = $_POST['name'];
+      $user_id = $_POST['user_id'];
+      $question = $_POST['question'];
+      $answer = $_POST['answer'];
+      $date_inserted = date("Y-m-d"); // current date
+      $ip = $_SERVER['REMOTE_ADDR'];
 
-// Tikrina ar yra keiksmazodziu zodyje
-$bad_words_sql = "SELECT curse_words FROM bad_words";
-$bad_words_result = mysqli_query($conn, $bad_words_sql);
-$bad_words_array = array();
+      $message = "Naujas klausimas sukurtas sėkmingai. Klausimas irašytas į laikinają duomenų bazę. Kur bus apdorojamas. Už įrašyta klausimą jums suteikta 10 LITŲ.";
+      if (empty($question) || empty($answer)) {
+          $message = "Klaida: ne visi laukai užpildyti. Klausimas ir atsakymas yra būtini.";
+      }
 
-if (mysqli_num_rows($bad_words_result) > 0) {
-    while($row = mysqli_fetch_assoc($bad_words_result)) {
-        $bad_words_array[] = $row["curse_words"];
-    }
-}
+      // Tikrina ar yra keiksmazodziu zodyje
+      $bad_words_sql = "SELECT curse_words FROM bad_words";
+      $bad_words_result = mysqli_query($conn, $bad_words_sql);
+      $bad_words_array = array();
 
-foreach ($bad_words_array as $bad_word) {
-    if (strpos($question, $bad_word) !== false) {
-        $message = "Klaida: klausime yra nepriimtinų žodžių.";
-        echo $message;
-        exit();
-    }
-}
+      if (mysqli_num_rows($bad_words_result) > 0) {
+          while($row = mysqli_fetch_assoc($bad_words_result)) {
+              $bad_words_array[] = $row["curse_words"];
+          }
+      }
 
-foreach ($bad_words_array as $bad_word) {
-    if (strpos($answer, $bad_word) !== false) {
-        $message = "Klaida: atsakyme yra nepriimtinų žodžių.";
-        echo $message;
-        exit();
-    }
-}
+      foreach ($bad_words_array as $bad_word) {
+          if (strpos($question, $bad_word) !== false) {
+              $message = "Klaida: klausime yra nepriimtinų žodžių.";
+              echo $message;
+              exit();
+          }
+      }
+
+      foreach ($bad_words_array as $bad_word) {
+          if (strpos($answer, $bad_word) !== false) {
+              $message = "Klaida: atsakyme yra nepriimtinų žodžių.";
+              echo $message;
+              exit();
+          }
+      }
 
 
-// Check if any of the fields are empty
-if (empty($question) || empty($answer)) {
-    echo $message;
-} else {
-    // Insert the data into the database
-    $sql = "INSERT INTO question_answer (user, super_users_id, question, answer, date_inserted, ip) VALUES ('$name', '$user_id', '$question', '$answer', '$date_inserted','$ip')";
-    if (mysqli_query($conn, $sql)) {
-        echo $message;
-        $sql = "UPDATE viktorina.super_users SET litai_sum = litai_sum + 10 WHERE nick_name = '$name'";
+      // Check if any of the fields are empty
+      if (empty($question) || empty($answer)) {
+          echo $message;
+      } else {
+          // Insert the data into the database
+          $sql = "INSERT INTO question_answer (user, super_users_id, question, answer, date_inserted, ip) VALUES ('$name', '$user_id', '$question', '$answer', '$date_inserted','$ip')";
+          if (mysqli_query($conn, $sql)) {
+              echo $message;
+              $sql = "UPDATE viktorina.super_users SET litai_sum = litai_sum + 10 WHERE nick_name = '$name'";
 
-        if (mysqli_query($conn, $sql)) {
-            echo "  Jums pervedama 10 LITŲ  ";
-        } else {
-            echo "Error updating litai_sum: " . mysqli_error($conn);
+              if (mysqli_query($conn, $sql)) {
+                  echo "  Jums pervedama 10 LITŲ  ";
+              } else {
+                  echo "Error updating litai_sum: " . mysqli_error($conn);
+              }
+
+          } else {
+              echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+          }
+      }
+
+      mysqli_close($conn);
+    ?>
+	</div>
+
+	<script>
+		let countdown = 25;
+    let timer = setInterval(function() {
+      countdown--;
+      document.getElementById("countdown").innerHTML = countdown;
+        if (countdown <= 0) {
+            clearInterval(timer);
+            window.location.href = "http://localhost/aldas/Viktorina.live/b_newquestionindex.php";
         }
+    }, 1000);
+	</script>
 
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-}
-
-mysqli_close($conn);
-
-echo "<script>setTimeout(function() { location.href = 'http://localhost/aldas/Viktorina.live/b_newquestionindex.php'; }, 30000);</script>";
-// echo $message;
-?>
+    <p>Jūs esate perkkeliamas atgal. Gal įrašysite dar vieną klausimą?  <span id="countdown">30</span> seconds.</p>
 
 
+</body>
+</html>
 
 
 
