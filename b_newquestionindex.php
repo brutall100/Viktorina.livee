3da176544d674680151567dec5f9aaf3ec385bf5
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-07-06 11:46:12 +0300

    V-4.5.1
    Ideta apsauga. Irasant klausima nebus 3 vienodu raidziu aaa ir klausimo ilgis max 25 simboliai

--- b_newquestionindex.php
+++ b_newquestionindex.php
@@ -21,88 +21,97 @@ $message = "";
 
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $host = 'localhost';
-    $user = 'root';
-    $password = '';
-    $dbname = 'viktorina';
+  $host = 'localhost';
+  $user = 'root';
+  $password = '';
+  $dbname = 'viktorina';
 
-    $conn = mysqli_connect($host, $user, $password, $dbname);
+  $conn = mysqli_connect($host, $user, $password, $dbname);
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
 
-    $name = $_POST['name'];
-    $user_id = $_POST['user_id'];
-    $question = $_POST['question'];
-    $answer = $_POST['answer'];
-    $date_inserted = date("Y-m-d"); // current date
-    $ip = $_SERVER['REMOTE_ADDR'];
+  $name = $_POST['name'];
+  $user_id = $_POST['user_id'];
+  $question = $_POST['question'];
+  $answer = $_POST['answer'];
+  $date_inserted = date("Y-m-d"); // current date
+  $ip = $_SERVER['REMOTE_ADDR'];
 
-    if (empty($question) || empty($answer)) {
-        $message = '<span class="empty-fields">Klaida:</br> Ne visi laukai užpildyti. Klausimas ir atsakymas yra būtini.</span>';
-    } else {
-        // Check if answer contains bad words
-        $bad_words_sql = "SELECT curse_words FROM bad_words";
-        $bad_words_result = mysqli_query($conn, $bad_words_sql);
-        $bad_words_array = array();
+  if (empty($question) || empty($answer)) {
+      $message = '<span class="empty-fields">Klaida:</br> Ne visi laukai užpildyti. Klausimas ir atsakymas yra būtini.</span>';
+  } else {
+      // Check if answer contains bad words
+      $bad_words_sql = "SELECT curse_words FROM bad_words";
+      $bad_words_result = mysqli_query($conn, $bad_words_sql);
+      $bad_words_array = array();
 
-        if (mysqli_num_rows($bad_words_result) > 0) {
-            while($row = mysqli_fetch_assoc($bad_words_result)) {
-                $bad_words_array[] = $row["curse_words"];
-            }
-        }
+      if (mysqli_num_rows($bad_words_result) > 0) {
+          while($row = mysqli_fetch_assoc($bad_words_result)) {
+              $bad_words_array[] = $row["curse_words"];
+          }
+      }
 
-        foreach ($bad_words_array as $bad_word) {
+      foreach ($bad_words_array as $bad_word) {
           if (strpos($question, $bad_word) !== false || strpos($answer, $bad_word) !== false) {
-                $message = '<span class="bad-word-message">Klaida:</br> Klausime arba atsakyme yra nepriimtinų žodžių.</span>';
-                break; // Stop the loop after finding the first bad word
-            }
-        }
+              $message = '<span class="bad-word-message">Klaida:</br> Klausime arba atsakyme yra nepriimtinų žodžių.</span>';
+              break; // Stop the loop after finding the first bad word
+          }
+      }
 
-        if (empty($message)) {
-            // Check user level
-            $check_level_sql = "SELECT user_lvl FROM super_users WHERE user_id = '$user_id'";
-            $check_level_result = mysqli_query($conn, $check_level_sql);
+      if (empty($message)) {
+          // Check for three consecutive letters
+          if (preg_match('/([a-zA-Z])\1{2}/', $question) || preg_match('/([a-zA-Z])\1{2}/', $answer)) {
+              $message = '<span class="consecutive-letters">Klaida:</br> Klausime arba atsakyme yra trys vienodi simboliai iš eilės.</span>';
+          } elseif (strlen($question) > 25 || strlen($answer) > 25) {
+              // Check word length
+              $message = '<span class="word-length-error">Klaida:</br> Klausimas arba atsakymas yra per ilgi. Maksimalus leidžiamas simbolių skaičius: 25.</span>';
+          } else {
+              // Check user level
+              $check_level_sql = "SELECT user_lvl FROM super_users WHERE user_id = '$user_id'";
+              $check_level_result = mysqli_query($conn, $check_level_sql);
 
-            if (mysqli_num_rows($check_level_result) > 0) {
-                $row = mysqli_fetch_assoc($check_level_result);
-                $user_lvl = $row['user_lvl'];
+              if (mysqli_num_rows($check_level_result) > 0) {
+                  $row = mysqli_fetch_assoc($check_level_result);
+                  $user_lvl = $row['user_lvl'];
 
-                if ($user_lvl <= 1) {
-                    $message = '<span class="user-level-error">Klaida:</br> Jūs neturite leidimo įrašyti klausimo ir atsakymo į duomenų bazę.</span>';
-                } else {
-                    // Check if the same question and answer already exist
-                    $check_sql = "SELECT * FROM question_answer WHERE question = '$question' AND answer = '$answer'";
-                    $check_result = mysqli_query($conn, $check_sql);
+                  if ($user_lvl <= 1) {
+                      $message = '<span class="user-level-error">Klaida:</br> Jūs neturite leidimo įrašyti klausimo ir atsakymo į duomenų bazę.</span>';
+                  } else {
+                      // Check if the same question and answer already exist
+                      $check_sql = "SELECT * FROM question_answer WHERE question = '$question' AND answer = '$answer'";
+                      $check_result = mysqli_query($conn, $check_sql);
 
-                    if (mysqli_num_rows($check_result) > 0) {
-                        $message = "Toks klausimas jau egzistuoja.";
-                    } else {
-                        // Insert the data into the database
-                        $sql = "INSERT INTO question_answer (user, super_users_id, question, answer, date_inserted, ip) VALUES ('$name', '$user_id', '$question', '$answer', '$date_inserted','$ip')";
-                        if (mysqli_query($conn, $sql)) {
-                            $message = '<span class="good-question">Naujas klausimas sukurtas sėkmingai. Klausimas irašytas į laikinają duomenų bazę. Kur bus balsuojama. Už įrašytą klausimą jums bus suteikta 10 LITŲ.</span>';
-                            $sql = "UPDATE super_users SET litai_sum = litai_sum + 10, timestamp_icon = CURRENT_TIMESTAMP WHERE nick_name = '$name'";
+                      if (mysqli_num_rows($check_result) > 0) {
+                          $message = "Toks klausimas jau egzistuoja.";
+                      } else {
+                          // Insert the data into the database
+                          $sql = "INSERT INTO question_answer (user, super_users_id, question, answer, date_inserted, ip) VALUES ('$name', '$user_id', '$question', '$answer', '$date_inserted','$ip')";
+                          if (mysqli_query($conn, $sql)) {
+                              $message = '<span class="good-question">Naujas klausimas sukurtas sėkmingai. Klausimas irašytas į laikinają duomenų bazę. Kur bus balsuojama. Už įrašytą klausimą jums bus suteikta 10 LITŲ.</span>';
+                              $sql = "UPDATE super_users SET litai_sum = litai_sum + 10, timestamp_icon = CURRENT_TIMESTAMP WHERE nick_name = '$name'";
 
-                            if (mysqli_query($conn, $sql)) {
-                                // Success
-                            } else {
-                                $message = "Error updating litai_sum: " . mysqli_error($conn);
-                            }
-                        } else {
-                            $message = "Error: " . $sql . "<br>" . mysqli_error($conn);
-                        }
-                    }
-                }
-            } else {
-                $message = "Nepavyko gauti naudotojo lygio informacijos.";
-            }
-        }
-    }
+                              if (mysqli_query($conn, $sql)) {
+                                  // Success
+                              } else {
+                                  $message = "Error updating litai_sum: " . mysqli_error($conn);
+                              }
+                          } else {
+                              $message = "Error: " . $sql . "<br>" . mysqli_error($conn);
+                          }
+                      }
+                  }
+              } else {
+                  $message = "Nepavyko gauti naudotojo lygio informacijos.";
+              }
+          }
+      }
+  }
 
-    mysqli_close($conn);
+  mysqli_close($conn);
 }
+
 ?>
 
 
