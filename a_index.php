5b848484bcfb13f564114a34ec73fcda3b51e74c
Vaidas <141738280+vatras7@users.noreply.github.com>
2023-08-09 10:29:28 +0300

    Add files via upload

--- a_index.php
+++ a_index.php
@@ -1,22 +1,42 @@
+ <?php
+if (session_status()!==2) { session_start();
+  foreach ($_POST as $key => $value) {
+    ${$key} = $value;
+    $_SESSION[$key] = $value;
+  }
+if (!empty($_SESSION)) {
+$name = $_SESSION['nick_name'];
+$passwd = $_SESSION['user_password'];
+$email = $_SESSION['user_email'] ?? '';
+$gender = $_SESSION['gender'] ?? $_['other_gender'] ?? '';
+}}
+// Siaip pasiziuret "?dev=1" prie adreso
+if (isset($_GET['dev']) && $_GET['dev']==1) {
+echo "<b>Sesijos ID: ".session_id()."<br>";
+echo session_name()."<br>";
+        echo '<pre>';
+        print_r($_SESSION);
+        echo  '</pre>';
+echo '<br>Request method: '. $_SERVER['REQUEST_METHOD']."</b><br>";
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <title>Viktorina.live</title>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="http://localhost/aldas/Viktorina.live/a_style.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&amp;display=swap" rel="stylesheet" /> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://localhost/Viktorina.live/a_style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"> 
     <link href="https://fonts.cdnfonts.com/css/neue-metana" rel="stylesheet"> 
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
   </head>
 
   <?php
-session_start();
-if (isset($_GET['name'])) {
-  $name = $_GET['name'];
+if (isset($name) & !empty($name)) {
   $conn = mysqli_connect("localhost", "root", "", "viktorina");
   $query = "SELECT user_lvl, litai_sum, user_id FROM super_users WHERE nick_name = '$name'";
   $result = mysqli_query($conn, $query);
@@ -26,11 +46,15 @@ if (isset($_GET['name'])) {
     $points = $row['litai_sum'];
     $user_id = $row['user_id'];
     mysqli_close($conn);
+    $_SESSION['user_id'] = $user_id;
+	$_SESSION['points'] = $points;
+	$_SESSION['user_lvl'] = $level;
   }
-  if (isset($_GET['level']) && isset($_GET['points'])) {
-    $level = $_GET['level'];
-    $points = $_GET['points'];
-  }
+      // Nereikia, istraukia auksciau
+  // if (isset($_GET['level']) && isset($_GET['points'])) {
+  //   $level = $_GET['level'];
+  //   $points = $_GET['points'];
+  // }
 }
 
 // if (isset($name)) {
@@ -40,9 +64,6 @@ if (isset($_GET['name'])) {
 // }
 
 ?>  
-
-
-
 
 <body>
 <div class="header-wrapper">
@@ -63,8 +84,7 @@ if (isset($_GET['name'])) {
     <div id="lita-bonus"></div>
 
     <?php
-if (isset($_GET['name'])) {
-  $name = $_GET['name'];
+if (isset($name)) {
   $conn = mysqli_connect("localhost", "root", "", "viktorina");
   $query = "SELECT user_lvl, litai_sum, litai_sum_today, user_id, (SELECT COUNT(*) FROM super_users WHERE litai_sum > su.litai_sum) + 1 AS position FROM super_users su WHERE nick_name = '$name'";
   $result = mysqli_query($conn, $query);
@@ -75,20 +95,14 @@ if (isset($_GET['name'])) {
     $points_today = $row['litai_sum_today'];
     $user_id = $row['user_id'];
     $position = $row['position'];
-    if (isset($_SESSION['name'])) {
       echo "Welcome, $name! Your level is $level. You have $points points. Your position in the database is $position.";
     }
-  } else {
-    echo "User not found!";
-  }
   mysqli_close($conn);
 } else {
-  echo "Labas! <img src='http://localhost/aldas/Viktorina.live/images/images_/smile.jpg' alt='Image description' style='width: 50px; height: auto;'> Norėdami pradėti rinkti Litus prisijunkite.";
+    echo "Labas! <img src='http://localhost/Viktorina.live/images/images_/smile.jpg' alt='Image description' style='width: 50px; height: auto;'> Norėdami pradėti rinkti Litus prisijunkite.";
 }
 ?>
 
-
-    
     <section class="today-top">
       <button class="today-top-btn" id="today-top-btn" >Šiandienos TOP 10</button>
     </section>
@@ -113,33 +127,40 @@ if (isset($_GET['name'])) {
         </div>  
       </div>
       <div class="form-container">
-        <?php if (isset($_SESSION['name']) && !empty($_SESSION['name']) && !isset($error)) { ?>
-          <form action="a_index.php?name=<?php echo isset($_GET['name']) ? urlencode($_GET['name']) : ''; ?>" id="answer-form" method="post">
+        <?php
+  $name = $name ?? "";
+  $conn = mysqli_connect("localhost", "root", "", "viktorina");
+  $query = "SELECT email_verified FROM super_users WHERE nick_name = '$name'";
+  $result = mysqli_query($conn, $query);
+  if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $verify = $row['email_verified'];
+   mysqli_close($conn); 
+}
+$verify = $verify ?? 0;
+        if (isset($name) && !empty($name) && !isset($error) && $verify==1) { ?>
+          <form action="a_index.php" id="answer-form" method="post">
             <div class="answer-input">
               <input type="text" id="answer-input" name="answer-input">
-              <input type="image" src="http://localhost/aldas/Viktorina.live/images/images_/send-btn-icon.png" alt="Submit" class="submit-icon">
+              <input type="image" src="http://localhost/Viktorina.live/images/images_/send-btn-icon.png" alt="Submit" class="submit-icon">
             </div>
           </form>
-        <?php } ?>
-      </div>
-
-
-
-
-      
+        <?php }
+	elseif(isset($name) && !empty($name) && !isset($error) && $verify==0)  echo "Patvirtinkite el.paštą :)";
+ ?>
+      </div>     
     </main>
-
-
     <section class="old-question-section">
       <div id="old-question"></div>
     </section>
 
-    <script type="text/javascript" src="http://localhost/aldas/Viktorina.live/a_index.js"></script>
+    <script type="text/javascript" src="http://localhost/Viktorina.live/a_index.js"></script>
     <footer class="footer">
       <object
-        data="http://localhost/aldas/Viktorina.live/Footer/footer.html"
+        data="http://localhost/Viktorina.live/Footer/footer.html"
         class="imported-footer">
       </object>
     </footer>
   </body>
+</html>>
 </html>
