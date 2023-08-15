f6f6511a56fbb238aa9bbbe7f33b6102f253c077
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-08-15 15:17:34 +0300

    V-4.8.8
    Moved all ;   Cleaner code some comments removed too.

--- d_server.js
+++ d_server.js
@@ -7,25 +7,25 @@ const { v4: uuidv4 } = require("uuid") // Generate UUIDs (npm install uuid)
 require("dotenv").config() // Load environment variables from .env file (no installation needed)
 const path = require("path") // Import the "path" module for file path manipulation (part of Node.js core)
 
-const { sendWelcomeEmail } = require("./d_mail");
-const app = express();
+const { sendWelcomeEmail } = require("./d_mail")
+const app = express()
 
-app.set("view engine", "ejs");// Set the directory for views/templates
-app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs") // Set the directory for views/templates
+app.set("views", path.join(__dirname, "views"))
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+  database: process.env.DB_DATABASE
+})
 
 connection.connect((err) => {
   if (err) throw err
-  console.log("Connected to database Viktorina");
+  console.log("Connected to database Viktorina")
 })
 
 //                     LOGIN
@@ -40,9 +40,7 @@ app.post("/login", (req, res) => {
       bcrypt.compare(user_password, hashedPassword, (err, match) => {
         if (err) throw err
         if (match) {
-          //res.redirect(`http://localhost/Viktorina.live/a_index.php?name=${nick_name}&email=${user_email}&level=${user_lvl}`)
-		  res.redirect(307,`http://localhost/Viktorina.live/a_index.php`)
-
+          res.redirect(307, `http://localhost/Viktorina.live/a_index.php`)
         } else {
           res.send("Invalid username or password")
         }
@@ -60,7 +58,7 @@ app.post("/register", (req, res) => {
   bcrypt.hash(user_password, 10, (err, hashedPassword) => {
     if (err) throw err
 
-    const currentDate = new Date().toISOString().slice(0, 20) // Get the current date in YYYY-MM-DD format
+    const currentDate = new Date().toISOString().slice(0, 20) 
 
     let genderValue = ""
     if (gender === "Other") {
@@ -85,10 +83,8 @@ app.post("/register", (req, res) => {
       } else {
         sendWelcomeEmail(nick_name, user_email, uuid)
 
-        const user_lvl = 0; // set the user_lvl variable to 0
-        //res.redirect(`http://localhost/Viktorina.live/a_index.php?name=${nick_name}&email=${user_email}&level=${user_lvl}`);
-		res.redirect(307,`http://localhost/Viktorina.live/a_index.php`);
-
+        const user_lvl = 0 
+        res.redirect(307, `http://localhost/Viktorina.live/a_index.php`)
       }
     })
   })
@@ -96,8 +92,8 @@ app.post("/register", (req, res) => {
 
 // Generate a random token
 function generateResetToken() {
-  return uuidv4();
-};
+  return uuidv4()
+}
 
 // Send password reset email using Nodemailer
 function sendResetEmail(email, token) {
@@ -152,93 +148,76 @@ app.post("/reset-password", (req, res) => {
 
 //                USER RESPOND, CLICK LINK and REDIRECTED TO reset-form.ejs
 app.get("/reset/:token", (req, res) => {
-  const resetToken = req.params.token;
+  const resetToken = req.params.token
 
-  // Query the database to find a user with the matching reset token
-  const findUserQuery = "SELECT * FROM super_users WHERE reset_token = ?";
-  
+  const findUserQuery = "SELECT * FROM super_users WHERE reset_token = ?"
+
   connection.query(findUserQuery, [resetToken], (error, results) => {
     if (error) {
-      console.error("Error querying database:", error);
-      return res.status(500).send("An error occurred.");
+      console.error("Error querying database:", error)
+      return res.status(500).send("An error occurred.")
     }
 
     if (results.length === 0) {
-      // No user found with the given reset token
-      return res.render("invalid-token", { errorMessage: "Turbūt bus pasibaiges nuorodos galiojimo laikas. Prašome pabandyti iš naujo" });
+      return res.render("invalid-token", { errorMessage: "Turbūt bus pasibaigęs nuorodos galiojimo laikas. Prašome pabandyti iš naujo" })
     }
-    
-    const user = results[0];
-    const resetTokenExpires = user.reset_token_expires;
-    
-    // Check if the reset token has expired
-    const now = new Date();
+
+    const user = results[0]
+    const resetTokenExpires = user.reset_token_expires
+
+    const now = new Date()
     if (resetTokenExpires < now) {
-      // Token has expired
-      return res.render("invalid-token", { errorMessage: "Pasibaiges nuorodos galiojimo laikas.Nuoroda galioja 1 parą.", yourEmailAddress: "viktorina.live@gmail.com" });
+      return res.render("invalid-token", { errorMessage: "Pasibaigęs nuorodos galiojimo laikas.Nuoroda galioja 1 parą.", yourEmailAddress: "viktorina.live@gmail.com" })
     }
-    
-    // Render the password reset form with the reset token
-    return res.render("reset-form", { token: resetToken });
-  });
-});
 
+    return res.render("reset-form", { token: resetToken })
+  })
+})
 
+//              USER ENTERS 2 PASW AND DB UPDATES
 app.post("/reset/:token", (req, res) => {
-  const token = req.params.token;
-  const newPassword = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+  const token = req.params.token
+  const newPassword = req.body.password
+  const confirmPassword = req.body.confirmPassword
 
-  console.log("Received token:", token);
-  console.log("New password:", newPassword);
-  console.log("Confirm password:", confirmPassword);
+  console.log("Received token:", token)
+  console.log("New password:", newPassword)
+  console.log("Confirm password:", confirmPassword)
 
   if (newPassword !== confirmPassword) {
-    console.log("Passwords do not match");
-    res.render("passw-dont-match");
-    return;
+    console.log("Passwords do not match")
+    res.render("passw-dont-match")
+    return
   }
 
   connection.query("SELECT * FROM super_users WHERE reset_token = ?", [token], (err, rows) => {
     if (err) {
-      console.error(err);
-      res.status(500).send("Internal server error");
-      return;
+      console.error(err)
+      res.status(500).send("Internal server error")
+      return
     }
 
     if (rows.length === 0) {
-      console.log("Invalid reset token");
-      res.status(400).send("Pasibaigęs rakto galiojimo laikas.");
-      return;
+      console.log("Invalid reset token")
+      res.status(400).send("Pasibaigęs rakto galiojimo laikas.")
+      return
     }
 
-    const user = rows[0];
-    const hashedPassword = bcrypt.hashSync(newPassword, 10);
+    const user = rows[0]
+    const hashedPassword = bcrypt.hashSync(newPassword, 10)
 
-    connection.query(
-      "UPDATE super_users SET user_password = ?, reset_token = NULL, reset_token_expires = NULL WHERE user_id = ?",
-      [hashedPassword, user.user_id],
-      (err, result) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Internal server error");
-          return;
-        }
-
-        console.log("Password updated successfully");
-        res.redirect("http://localhost/Viktorina.live/d_regilogi.php"); 
+    connection.query("UPDATE super_users SET user_password = ?, reset_token = NULL, reset_token_expires = NULL WHERE user_id = ?", [hashedPassword, user.user_id], (err, result) => {
+      if (err) {
+        console.error(err)
+        res.status(500).send("Internal server error")
+        return
       }
-    );
-  });
-});
 
-
-// app.get("/reset-form/:token", (req, res) => {
-//   const token = req.params.token;
-//   res.render("reset-form.ejs", { token });
-// });
-
-
+      console.log("Password updated successfully")
+      res.redirect("http://localhost/Viktorina.live/d_regilogi.php")
+    })
+  })
+})
 
 
 
@@ -260,7 +239,7 @@ app.get("/confirm", (req, res) => {
 })
 
 // Start the server
-const PORT = process.env.PORT4;
+const PORT = process.env.PORT4
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+  console.log(`Server listening on port ${PORT}`)
+})
