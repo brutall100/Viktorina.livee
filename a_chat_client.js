5bb5e59d8e9738d3bba81bd52af639597c5757ac
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-10-01 16:10:43 +0300

    V-5.5.9
    Removed local storage. i dont need anymore.

--- a_chat_client.js
+++ a_chat_client.js
@@ -7,23 +7,18 @@ console.log("Username:", username)
 console.log("Chat User ID:", chatUserId)
 console.log("User Level:", userLvl)
 
-// Function to determine message limit based on user level
 function getMessageLimit(userLvl) {
-  // Define message limits for each user level in an object
   const messageLimits = {
     0: 8,
     1: 20,
     2: 35,
     3: 50,
     4: 100,
-    5: 100,
-  };
-  // If the user level is not found in the object, return a default limit (e.g., 100)
-  return messageLimits.hasOwnProperty(userLvl) ? messageLimits[userLvl] : 100;
+    5: 100
+  }
+  return messageLimits.hasOwnProperty(userLvl) ? messageLimits[userLvl] : 100
 }
 
-
-// Function to scroll to the bottom of the chat messages
 function scrollToBottom() {
   const chatMessages = document.getElementById("chat-container-messages")
   console.log(chatMessages)
@@ -38,129 +33,76 @@ function addMessageToChat(user_message) {
   newMessageItem.textContent = `${username}: ${user_message}`
   chatMessages.appendChild(newMessageItem)
 
-  // Save the user_message to local storage
-  saveMessageToLocal(user_message)
-
-  // Scroll to the bottom after adding a user_message
   scrollToBottom()
   console.log("Added user_message to chat")
 }
 
-// Function to save a user_message to local storage with a 20-message limit
-function saveMessageToLocal(user_message) {
-  let storedMessages = localStorage.getItem("chatMessages") || []
-
-  if (typeof storedMessages === "string") {
-    storedMessages = JSON.parse(storedMessages)
-  }
-
-  // Push the new user_message to the end of the array
-  storedMessages.push(`${username}: ${user_message}`)
-
-  // Limit the number of stored messages to 20
-  if (storedMessages.length > 100) {
-    storedMessages = storedMessages.slice(-100)
-  }
-
-  // Store the updated messages back to local storage
-  localStorage.setItem("chatMessages", JSON.stringify(storedMessages))
-  console.log("Saved user_message to local storage")
-}
-
-// Function to load messages from local storage and display them
-function loadMessagesFromLocal() {
-  const chatMessages = document.getElementById("chat-messages")
-  chatMessages.innerHTML = "" // Clear existing messages
-
-  let storedMessages = localStorage.getItem("chatMessages") || []
-  if (typeof storedMessages === "string") {
-    storedMessages = JSON.parse(storedMessages)
-  }
-
-  // Determine the maximum number of messages to display based on user level
-  const maxMessages = getMessageLimit(userLvl) // Use the function here
-
-  // Display the stored messages in the chat (limited by maxMessages)
-  storedMessages.slice(-maxMessages).forEach((user_message) => {
-    const newMessageItem = document.createElement("li")
-    newMessageItem.textContent = user_message
-    chatMessages.appendChild(newMessageItem)
-  })
-
-  console.log("Loaded messages from local storage")
-}
-
 // Function to handle sending a user_message
-let canSendMessage = true; // Flag to control message sending
-let sendButtonTimeout = null; // Timeout reference
+let canSendMessage = true
+let sendButtonTimeout = null
 
 function sendMessage(event) {
   if (event) {
-    event.preventDefault(); // Prevent form submission and page reload
+    event.preventDefault()
   }
 
   if (!canSendMessage) {
-    console.log("You can send a message every 5 seconds.");
-    return;
+    console.log("You can send a message every 5 seconds.")
+    return
   }
 
-  const inputElement = document.getElementById("chat-input-msg");
-  const user_message = inputElement.value.trim();
+  const inputElement = document.getElementById("chat-input-msg")
+  const user_message = inputElement.value.trim()
 
-  const userIdInput = document.getElementById("chat-user-id");
-  const user_id = userIdInput.value;
+  const userIdInput = document.getElementById("chat-user-id")
+  const user_id = userIdInput.value
 
-  const userNameInput = document.getElementById("chat-user-name");
-  const user_name = userNameInput.value;
+  const userNameInput = document.getElementById("chat-user-name")
+  const user_name = userNameInput.value
 
   if (user_message !== "") {
-    // Disable the send button
-    chatButton.disabled = true;
+    chatButton.disabled = true // Disable the send button
 
-    addMessageToChat(user_message);
+    addMessageToChat(user_message)
 
     // Send the user_message to the server
     fetch("http://localhost:4005/save-message", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         message: user_message,
         user_id: user_id,
-        user_name: user_name,
-      }),
+        user_name: user_name
+      })
     })
       .then((response) => {
         if (response.ok) {
-          console.log("Message sent to server successfully");
-          canSendMessage = false; // Disable sending for 5 seconds
+          console.log("Message sent to server successfully")
+          canSendMessage = false
 
-          // Set a timeout to re-enable the send button after 5 seconds
           sendButtonTimeout = setTimeout(() => {
-            canSendMessage = true; // Allow sending
-            chatButton.disabled = false; // Re-enable the send button
-          }, 5000); // 5 seconds
+            canSendMessage = true
+            chatButton.disabled = false
+          }, 10000) // 10 seconds   // Set a timeout to re-enable the send button after 10 seconds
         } else {
-          console.error("Failed to send message");
-          chatButton.disabled = false; // Re-enable the send button on error
+          console.error("Failed to send message")
+          chatButton.disabled = false
         }
       })
       .catch((error) => {
-        console.error("Error sending message:", error);
-        chatButton.disabled = false; // Re-enable the send button on error
-      });
+        console.error("Error sending message:", error)
+        chatButton.disabled = false
+      })
 
-    inputElement.value = "";
+    inputElement.value = ""
   }
 }
 
-
-// Event listener for the "Send" button
 const chatButton = document.getElementById("chat-button")
 chatButton.addEventListener("click", sendMessage)
 
-// Event listener for pressing Enter in the input field
 const chatInput = document.getElementById("chat-input-msg")
 chatInput.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
@@ -168,52 +110,38 @@ chatInput.addEventListener("keyup", (event) => {
   }
 })
 
-// Function to load messages from the server and display them in reverse order
+// Function to load messages from the server and display them
 async function loadMessagesFromServer() {
   try {
-    const response = await fetch("http://localhost:4005/get-older-messages")
+    const response = await fetch("http://localhost:4005/get-older-messages");
     if (!response.ok) {
-      throw new Error("Failed to fetch messages from the server")
+      throw new Error("Failed to fetch messages from the server");
     }
 
-    const messages = await response.json()
+    const messages = await response.json();
 
-    const chatMessages = document.getElementById("chat-messages")
+    const chatMessages = document.getElementById("chat-messages");
 
-    // Determine message limits based on user level
-    const messageLimit = getMessageLimit(userLvl)
+    const messageLimit = getMessageLimit(userLvl);
 
-    // Reverse the order of messages and then display them
-    messages.reverse().forEach(({ chat_msg, chat_user_name }, index) => {
-      // Check if the message is not in local storage and doesn't exceed the message limit before displaying it
-      if (index < messageLimit && !isMessageInLocalStorage(`${chat_user_name}: ${chat_msg}`)) {
-        const newMessageItem = document.createElement("li")
-        newMessageItem.textContent = `${chat_user_name}: ${chat_msg}`
-        chatMessages.appendChild(newMessageItem)
-      }
-    })
+    // Filter and display messages based on the user's level
+    const messagesToDisplay = messages
+      .slice(-messageLimit) // Get the latest 'messageLimit' messages
+      .reverse(); // Reverse the order to display the newest first
 
-    console.log("Loaded messages from the server")
+    messagesToDisplay.forEach(({ chat_msg, chat_user_name }) => {
+      const newMessageItem = document.createElement("li");
+      newMessageItem.textContent = `${chat_user_name}: ${chat_msg}`;
+      chatMessages.appendChild(newMessageItem);
+    });
+
+    console.log("Loaded messages from the server");
   } catch (error) {
-    console.error("Error loading messages from the server:", error)
+    console.error("Error loading messages from the server:", error);
   }
 }
 
-// Function to check if a message is in local storage
-function isMessageInLocalStorage(message) {
-  let storedMessages = localStorage.getItem("chatMessages") || []
-
-  if (typeof storedMessages === "string") {
-    storedMessages = JSON.parse(storedMessages)
-  }
-
-  // Check if the message is in the array
-  return storedMessages.includes(message)
-}
-
-// Modify the window load event listener
 window.addEventListener("load", async () => {
   await loadMessagesFromServer()
-  loadMessagesFromLocal()
   scrollToBottom()
 })
--- a_chat_server.js
+++ a_chat_server.js
@@ -16,19 +16,16 @@ const db = mysql.createPool({
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Define a route to handle saving messages
 app.post("/save-message", async (req, res) => {
   try {
-    const { user_id, message, user_name } = req.body // Extract the 'message' property from the request body
+    const { user_id, message, user_name } = req.body
     const currentDate = moment().format("YYYY-MM-DD HH:mm:ss")
 
-    // Insert the message into the database
     const [result] = await db.execute("INSERT INTO chat_app_db (chat_id, chat_user_id, chat_msg, chat_user_name, chat_date) VALUES (NULL, ?, ?, ?, ?)", [user_id, message, user_name, currentDate])
 
     console.log("Query result:", result)
 
     if (result.affectedRows === 1) {
-      // If the message was saved successfully, send a 200 OK response
       res.status(200).json({ message: "Message saved successfully" })
     } else {
       console.error("Error saving message:", result.message)
@@ -40,12 +37,11 @@ app.post("/save-message", async (req, res) => {
   }
 })
 
-// Function to check the message count in the database and delete the oldest messages if the count exceeds a limit
 async function checkAndDeleteOldestMessagesIfNeeded() {
   try {
     const [countRows] = await db.execute("SELECT COUNT(*) AS messageCount FROM chat_app_db")
 
-    const maxMessageCount = 10000
+    const maxMessageCount = 999
 
     const messageCount = countRows[0].messageCount
 
@@ -63,13 +59,13 @@ async function checkAndDeleteOldestMessagesIfNeeded() {
 
 app.get("/get-older-messages", async (req, res) => {
   try {
-    // Call the function to check and delete oldest messages
     await checkAndDeleteOldestMessagesIfNeeded()
 
-    // Query the database to fetch the 100 oldest messages
     const [rows] = await db.execute("SELECT chat_msg, chat_user_name FROM chat_app_db ORDER BY chat_date DESC LIMIT 100")
 
-    // Send the fetched messages as a response
+    // Log the messages before sending them to the client
+    console.log("Messages sent to client:", rows)
+
     res.status(200).json(rows)
   } catch (error) {
     console.error("Error fetching older messages:", error)
