af8e589aa659cdcf908fbea22ecbb4af847a4708
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-11-06 16:04:03 +0200

    V-6.0.0
    Galima pasikeisti varda i belenkoki ir nuima 10 000 litu kolkas.

--- Info/Mano_info/i_myInfoServer.js
+++ Info/Mano_info/i_myInfoServer.js
@@ -32,25 +32,33 @@ app.post("/updateName", async (req, res) => {
       // Check if the user with the old name, userId, and userLitai exists
       const [userRows] = await connection.execute("SELECT * FROM super_users WHERE nick_name = ? AND user_id = ? AND litai_sum = ?", [userName, userId, userLitai])
 
-      if ((newName.length === userName.length + 1 || newName.length === userName.length - 1) && (newName.startsWith(userName.slice(0, -1)) || newName.startsWith(userName + " "))) {
+      if (userRows.length === 0) {
+        res.status(400).json({ message: "Vartotojas su seno vardo, ID ir litai nerastas" })
+      } else {
         // Check if the new name contains disallowed words from the same database
         const [badWordsRows] = await connection.execute("SELECT * FROM bad_words WHERE ? LIKE CONCAT('%', curse_words, '%')", [newName])
 
         if (badWordsRows.length > 0) {
-          const disallowedWord = badWordsRows[0].curse_words
+          const disallowedWord = badWordsRows[0].curse_words // Assuming only one disallowed word for simplicity
           console.log(`Warning: User name '${newName}' contains disallowed word '${disallowedWord}'`)
           res.status(400).json({ message: `Vartotojo vardas negali turėti neleistino žodžio: ${disallowedWord}` })
         } else {
+          // Update the name if the user with the old name, userId, and userLitai exists
           const [updateRows] = await connection.execute("UPDATE super_users SET nick_name = ? WHERE user_id = ? AND litai_sum = ?", [newName, userId, userLitai])
 
           if (updateRows.affectedRows > 0) {
-            res.json({ message: `Jūsų naujasis vardas ${newName}` })
+            // Subtract 10,000 from litai_sum
+            const [subtractRows] = await connection.execute("UPDATE super_users SET litai_sum = litai_sum - 10000 WHERE user_id = ?", [userId])
+
+            if (subtractRows.affectedRows > 0) {
+              res.json({ message: `Jūsų naujasis vardas ${newName}` })
+            } else {
+              res.status(400).json({ message: "Nepavyko atnaujinti vardo" })
+            }
           } else {
             res.status(400).json({ message: "Nepavyko atnaujinti vardo" })
           }
         }
-      } else {
-        res.status(400).json({ message: "Naujas vardas turi būti tik vieno simbolio pailginimas arba sutrumpinimas senojo vardo" })
       }
     }
 
