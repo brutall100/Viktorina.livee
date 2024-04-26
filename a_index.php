3143e8a7e1e0128e8ac6fa1bb3be4b1bcd7df75c
Aldas <brutall100@gmail.com>
2024-04-26 14:50:45 +0300

    V-7.9.0
    Completely changed structure of main page. To flexbox.

--- a_index.php
+++ a_index.php
@@ -68,6 +68,8 @@ if (isset($name) & !empty($name)) {
 }
 ?>
 
+
+
 <body>
   <div class="header-wrapper">
     <?php include 'Header/header.php'; ?>
@@ -88,125 +90,134 @@ if (isset($name) & !empty($name)) {
   <!-- <div id="dataContainer"></div> -->
 
 
+  <div class="containers-abc">  <!--start of  A B C container -->
+    <div class="container-a">                                         <!-- Start of A  -->
+    <!-- A1 -->
+      <div class="user-info-container">
+        <?php
+        if (isset($name)) {
+            include 'x_configDB.php';
 
-  <?php
-    if (isset($name)) {
-        include 'x_configDB.php';
-
-        $query = "SELECT user_lvl, litai_sum, litai_sum_today, user_id, (SELECT COUNT(*) FROM super_users WHERE litai_sum > su.litai_sum) + 1 AS position FROM super_users su WHERE nick_name = '$name'";
-        $result = mysqli_query($conn, $query);
-        if (mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
-            $level = $row['user_lvl'];
-            $points = $row['litai_sum'];
-            $points_today = $row['litai_sum_today'];
-            $user_id = $row['user_id'];
-            $position = $row['position'];
-            echo "<div class='user-info'>";
-            echo "<div class='welcome-message'><span id='temp-greeting'>Labas,</span> $name <span id='temp-exclamation'>!</span></div>";
-            echo "<div class='points-info'>Vieta tope <span class='position-highlight'>$position</span>.</div>";
-            echo "<div class='points-info'>Turite <span class='points-highlight'>$points LT</span>.</div>";
-            echo "<div class='points-info'>Lygis <span class='level-highlight'>$level</span>.</div>";
+            $query = "SELECT user_lvl, litai_sum, litai_sum_today, user_id, (SELECT COUNT(*) FROM super_users WHERE litai_sum > su.litai_sum) + 1 AS position FROM super_users su WHERE nick_name = '$name'";
+            $result = mysqli_query($conn, $query);
+            if (mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+                $level = $row['user_lvl'];
+                $points = $row['litai_sum'];
+                $points_today = $row['litai_sum_today'];
+                $user_id = $row['user_id'];
+                $position = $row['position'];
+                echo "<div class='user-info'>";
+                echo "<div class='welcome-message'><span id='temp-greeting'>Labas,</span> $name <span id='temp-exclamation'>!</span></div>";
+                echo "<div class='points-info'>Vieta tope <span class='position-highlight'>$position</span>.</div>";
+                echo "<div class='points-info'>Turite <span class='points-highlight'>$points LT</span>.</div>";
+                echo "<div class='points-info'>Lygis <span class='level-highlight'>$level</span>.</div>";
+                echo "</div>";
+            }
+            mysqli_close($conn);
+        } else {
+            echo "<div class='user-info'>"; 
+            echo "<div class='greeting-message'>
+                      Labas! 
+                      <img src='images/images_/smile.jpg'
+                          alt='Šypsenėlė su tekstu.' 
+                          aria-label='Šypsenėlė su tekstu, kuriame sakoma, kad norint žaisti reikia prisijungti.'> 
+                      Norėdami pradėti rinkti Litus prisijunkite.
+                  </div>";
             echo "</div>";
         }
-        mysqli_close($conn);
-    } else {
-        echo "<div class='user-info'>"; 
-        echo "<div class='greeting-message'>
-                  Labas! 
-                  <img src='images/images_/smile.jpg'
-                      alt='Šypsenėlė su tekstu, kuriame sakoma, kad norint žaisti reikia prisijungti.' 
-                      aria-label='Šypsenėlė su tekstu, kuriame sakoma, kad norint žaisti reikia prisijungti.'> 
-                  Norėdami pradėti rinkti Litus prisijunkite.
-              </div>";
-        echo "</div>";
-    }
-   ?>
+        ?>
+     </div>
+     <!-- A2 -->
+     <div class="show-money-container">  <!-- Sie 2 conteineriai turi susieiti i viena . Vienas be kito negali-->
+        
+          <div id="lita"></div>
+          <div id="lita-bonus"></div>
 
-
-  <div class="show-money-container">
-    <div id="lita"></div>
-    <div id="lita-bonus"></div>
-  </div>
-
-
-  <section class="today-top">
-    <button type="button" class="today-top-btn" id="today-top-btn">Šiandienos TOP 10</button>
-  </section>
-
-  <section class="litas-container">
-    <div class="litas-container-img" id="litai-img-bonus"></div>
-    <div class="litas-container-img" id="litai-img"></div>
-  </section>
-
-  <main class="super-container">
-      <div class="points-container">
-          <div id="points"></div>
-          <div id="bonus-points"></div>
+        <section class="litas-container">
+          <div class="litas-container-img" id="litai-img-bonus"></div>
+          <div class="litas-container-img" id="litai-img"></div>
+        </section>
+     </div>
+     <!-- A3 -->
+     <div id="chat-container-section">
+      <div id="chat-container-messages">
+        <ul id="chat-messages"></ul>
+        <div id="chat-user-data" chat-data-name="<?php echo isset($name) ? $name : ''; ?>"></div>
       </div>
-
-      <div class="super-container-qna-section">
-          <div id="question"></div>
-          <div id="answer"></div>
-          <div class="answer-section">
-              <div id="answer-msg"></div>
-          </div>
+      <div>
+        <form id="chat-form">
+          <input id="chat-input-msg" name="user_message" autocomplete="off">
+          <input type="hidden" id="chat-user-id" name="user_id" value="<?php echo isset($user_id) ? $user_id : ''; ?>">
+          <input type="hidden" id="chat-user-name" name="user_name" value="<?php echo isset($name) ? $name : ''; ?>">
+          <input type="hidden" id="chat-user-level" name="user_level" value="<?php echo isset($level) ? $level : ''; ?>">
+          <button type="submit" id="chat-button">Send</button>
+        </form>
       </div>
-
-      <div class="form-container">
-          <?php
-          $name = $name ?? "";
-          include 'x_configDB.php';
-          $query = "SELECT email_verified FROM super_users WHERE nick_name = '$name'";
-          $result = mysqli_query($conn, $query);
-          $verify = 0;  // Default to not verified
-          
-          if (mysqli_num_rows($result) > 0) {
-              $row = mysqli_fetch_assoc($result);
-              $verify = $row['email_verified'];
-          }
-          mysqli_close($conn);
-          
-          if (!empty($name) && $verify == 1) { ?>
-              <form action="a_index.php" id="answer-form" method="post">
-                  <div class="answer-input">
-                      <input type="text" id="answer-input" name="answer-input">
-                      <input type="image" src="images/images_/send-btn-icon.png" alt="Submit" class="submit-icon">
-                  </div>
-              </form>
-          <?php } elseif (!empty($name) && $verify == 0) { ?>
-              <div class="email-confirmation-message">
-                <p>Prašome patikrinti savo el. paštą ir paspausti nuorodą, kad galėtumėte dalyvauti 
-                    <span class="email-confirmation-span">Viktorinos</span> žaidime. 
-                    <a href="mailto:" class="email-confirmation-link">Jūsų el. paštas</a>
-                </p>
-              </div>
-          <?php } ?>
-      </div>
-
-  </main>
-
-
-
-  <div id="chat-container-section">
-    <div id="chat-container-messages">
-      <ul id="chat-messages"></ul>
-      <div id="chat-user-data" chat-data-name="<?php echo isset($name) ? $name : ''; ?>"></div>
     </div>
-    <div>
-      <form id="chat-form">
-        <input id="chat-input-msg" name="user_message" autocomplete="off">
-        <input type="hidden" id="chat-user-id" name="user_id" value="<?php echo isset($user_id) ? $user_id : ''; ?>">
-        <input type="hidden" id="chat-user-name" name="user_name" value="<?php echo isset($name) ? $name : ''; ?>">
-        <input type="hidden" id="chat-user-level" name="user_level" value="<?php echo isset($level) ? $level : ''; ?>">
-        <button type="submit" id="chat-button">Send</button>
-      </form>
-    </div>
-  </div>
 
-  <div class="old-question-section">
-    <div id="old-question"></div>
-  </div>
+   </div>                                                           <!-- END of A -->
+
+   <div class="container-b">                                       <!-- Start of B  -->
+   <!-- B1 -->
+      <main class="super-container">
+            <div class="points-container">
+                <div id="points"></div>
+                <div id="bonus-points"></div>
+            </div>
+
+            <div class="super-container-qna-section">
+                <div id="question"></div>
+                <div id="answer"></div>
+                <div class="answer-section">
+                    <div id="answer-msg"></div>
+                </div>
+            </div>
+
+            <div class="form-container">
+                <?php
+                $name = $name ?? "";
+                include 'x_configDB.php';
+                $query = "SELECT email_verified FROM super_users WHERE nick_name = '$name'";
+                $result = mysqli_query($conn, $query);
+                $verify = 0;  // Default to not verified
+                
+                if (mysqli_num_rows($result) > 0) {
+                    $row = mysqli_fetch_assoc($result);
+                    $verify = $row['email_verified'];
+                }
+                mysqli_close($conn);
+                
+                if (!empty($name) && $verify == 1) { ?>
+                    <form action="a_index.php" id="answer-form" method="post">
+                        <div class="answer-input">
+                            <input type="text" id="answer-input" name="answer-input">
+                            <input type="image" src="images/images_/send-btn-icon.png" alt="Submit" class="submit-icon">
+                        </div>
+                    </form>
+                <?php } elseif (!empty($name) && $verify == 0) { ?>
+                    <div class="email-confirmation-message">
+                      <p>Prašome patikrinti savo el. paštą ir paspausti nuorodą, kad galėtumėte dalyvauti 
+                          <span class="email-confirmation-span">Viktorinos</span> žaidime. 
+                          <a href="mailto:" class="email-confirmation-link">Jūsų el. paštas</a>
+                      </p>
+                    </div>
+                <?php } ?>
+            </div>
+        </main>
+   </div>                                                     <!-- END of B -->
+
+   <div class="container-c">                                  <!-- Start of C  -->
+   <!-- C1 -->
+      <section class="today-top">
+        <button type="button" class="today-top-btn" id="today-top-btn">Šiandienos TOP 10</button>
+      </section>
+     <!-- C2  -->
+    <div class="old-question-section">
+      <div id="old-question"></div>
+    </div>
+
+  </div> <!-- END of containers-abc -->
 
   <div class="footer-wrapper">
     <?php include './Footer/footer.php'; ?>
