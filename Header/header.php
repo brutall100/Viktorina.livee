b4ef3dd3dc37d4565010f0499070437d4f7e6847
Aldas <brutall100@gmail.com>
2024-07-20 09:30:57 +0300

    Some Header update. Added some  empty links

--- Header/header.php
+++ Header/header.php
@@ -9,33 +9,58 @@
                 <img class="logo" src="/viktorina.live/images/icons/VK_new3.png" alt="viktorina-logo"
                     aria-label="Viktorina logotipas, Išpuošta V raidė Simbolizuoja Viktorina.live">
             </div>
-            <a class="navbar-brand cs-navbar-brand" href="#">𝕍𝕀𝕂𝕋𝕆ℝ𝕀ℕ𝔸.𝕃𝕀𝕍𝔼</a>
+            <a class="navbar-brand cs-navbar-brand" href="/viktorina.live/a_index.php">𝕍𝕀𝕂𝕋𝕆ℝ𝕀ℕ𝔸.𝕃𝕀𝕍𝔼</a>
             <button class="navbar-toggler cs-bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav justify-content-center w-100 cs-navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link fs-5 fw-semibold cs-anchor" aria-current="page" href="/viktorina.live/a_index.php">Viktorina</a>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link fs-5 fw-semibold cs-anchor dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Viktorina
+                        </a>
+                        <ul class="dropdown-menu mb-2">
+                            <li><a class="dropdown-item" href="/viktorina.live/a_index.php">Klasikinė</a></li>
+                            <li><a class="dropdown-item" href="#">Šeimos..kuriama</a></li>
+                            <li><a class="dropdown-item" href="#">Grandinėlė..kuriama</a></li>
+                            <li><a class="dropdown-item" href="#">Golden mind..kuriama</a></li>
+                        </ul>
                     </li>
+
                     <li class="nav-item">
                         <a class="nav-link fs-5 fw-semibold cs-anchor" href="/viktorina.live/c_questionwaiting.php">Balsavimas</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link fs-5 fw-semibold cs-anchor" href="/viktorina.live/b_newquestionindex.php">Irašyti klausimą</a>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link fs-5 fw-semibold cs-anchor dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Įrašyti klausimą
+                        </a>
+                        <ul class="dropdown-menu mb-2">
+                            <li><a class="dropdown-item" href="/viktorina.live/b_newquestionindex.php">Klasikinė viktorina</a></li>
+                            <li><a class="dropdown-item" href="#">Šeimos viktorina</a></li>
+                            <li><a class="dropdown-item" href="#">Option 3</a></li>
+                            <li><a class="dropdown-item" href="#">Option 4</a></li>
+                        </ul>
                     </li>
+
                     <li class="nav-item dropdown">
                         <a class="nav-link fs-5 fw-semibold cs-anchor dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
                             Info
                         </a>
                         <ul class="dropdown-menu mb-2">
-                            <li><a class="dropdown-item" href="/viktorina.live/Info/Mano_info/i_myInfo.php">Mano info</a></li>
-                            <li><a class="dropdown-item" href="/viktorina.live/Info/Puslapio_klaidos/i_page_mistake.php">Puslapio klaidos</a></li>
+                            <li><a class="dropdown-item" href="/viktorina.live/Info/Mano_info/i_myInfo.php">Mano
+                                    info</a></li>
+                            <li><a class="dropdown-item" href="/viktorina.live/Info/Puslapio_klaidos/i_page_mistake.php">Puslapio klaidos</a>
+                            </li>
                             <li><a class="dropdown-item" href="/viktorina.live/Info/Balsavimas/i_vote.php">Balsavimas</a></li>
                             <li><a class="dropdown-item" href="/viktorina.live/Info/Pasiulymai/i_minds.php">Pasiūlymai</a></li>
-                            <li><a class="dropdown-item" href="/viktorina.live/Info/Klaidos_klausime/i_qna_mistake.php">Klaidos klausime</a></li>
+                            <li><a class="dropdown-item" href="/viktorina.live/Info/Klaidos_klausime/i_qna_mistake.php">Klaidos klausime</a>
+                            </li>
                             <li>
                                 <div class="dropdown-submenu-svarbu mb-1">
                                     <a href="#" class="dropdown-item ">Svarbu</a>
@@ -58,19 +83,22 @@
             </div>
         </div>
     </nav>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            document.querySelectorAll('.dropdown-submenu-svarbu').forEach(function (element) {
-                element.addEventListener('mouseover', function (e) {
+        document.addEventListener('DOMContentLoaded', function() {
+            document.querySelectorAll('.dropdown-submenu-svarbu').forEach(function(element) {
+                element.addEventListener('mouseover', function(e) {
                     let submenu = this.querySelector('.dropdown-menu');
                     if (submenu) {
                         submenu.classList.add('show');
                     }
                 });
-
-                element.addEventListener('mouseout', function (e) {
+                element.addEventListener('mouseout', function(e) {
                     let submenu = this.querySelector('.dropdown-menu');
                     if (submenu) {
                         submenu.classList.remove('show');
@@ -85,35 +113,15 @@
     const logoutButton = document.getElementById('btn-atsijungti');
     const name = "<?php echo $name ?>";
     logoutButton.addEventListener('click', () => {
-        window.location.href = `./viktorina.live/Statistica/statistic.php`;
+        window.location.href = `/viktorina.live/Statistica/statistic.php`;
     });
 </script>
 <?php else : ?>
 <script>
     function redirectToLogin() {
-        window.location.href = `./viktorina.live/d_regilogi.php`;
+        // window.location.href = `/viktorina.live/d_regilogi.php`;
+        window.location.href = `/`;
     }
 </script>
 
 <?php endif; ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
